add1586d19caf6ca68aa2f53987109b455a1aba6
George Spake <gpspake@gmail.com>
2017-03-01 10:29:33 -0600

    add uthsc banner template part

---
+++ emerald-wp/template-parts/uthsc-banner.php
@@ -0,0 +1,14 @@
+<header aria-label="UTHSC Logo" role="banner" class="uthsc-banner hide-for-print">
+    <div class="row">
+        <div class="medium-10 large-8 small-centered columns">
+            <a href="http://uthsc.edu">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/uthsc-logo-white-text.svg'?>" alt="UTHSC logo" class="uthsc-logo"/>
+            </a>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="hide-for-large columns small-collapse">
+        </div>
+    </div>
+</header>
