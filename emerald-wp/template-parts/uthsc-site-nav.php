7befc3aaeb159b69f5070a4b2bf3797c8dc83df5
George Spake <gpspake@gmail.com>
2017-03-01 10:28:44 -0600

    add site nav template part

---
+++ emerald-wp/template-parts/uthsc-site-nav.php
@@ -0,0 +1,72 @@
+<nav class="uthsc-site-nav hide-for-print" aria-label="UTHSC site menu" role="navigation">
+    <div class="uthsc-site-nav-left">
+        <ul class="menu">
+            <li><a href="/students/">Students</a></li>
+            <li><a href="/faculty/">Faculty &amp; Staff</a></li>
+            <li><a href="/alumni/">Alumni</a></li>
+            <li><a href="/visitors/">Visitors</a></li>
+            <li><a href="/clinicalcare/">Patients</a></li>
+        </ul>
+    </div>
+
+    <div class="uthsc-site-nav-right">
+        <ul class="dropdown menu" data-dropdown-menu="data-dropdown-menu">
+            <li class="has-submenu">
+                <a href="#">Our Mission</a>
+                <ul class="submenu menu vertical" data-submenu="data-submenu">
+                    <li>
+                        <a href="/education/">
+                            <span aria-hidden="true" class="fa fa-graduation-cap"></span>
+                            Academics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/redesign/research/">
+                            <span aria-hidden="true" class="fa fa-flask"></span>
+                            Research
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/clinicalcare/">
+                            <span aria-hidden="true" class="fa fa-medkit"></span>
+                            Clinical Care
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/publicservice/">
+                            <span aria-hidden="true" class="fa fa-globe"></span>
+                            Public Service
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/aboututhsc/utmission.php">
+                            Mission Statement
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class="has-submenu">
+                <a href="#">Resources</a>
+                <ul class="submenu menu vertical" data-submenu>
+                    <li><a href="/banner/info/">Banner</a></li>
+                    <li><a href="https://courses.uthsc.edu/">Blackboard</a></li>
+                    <li><a href="http://events.uthsc.edu/">Calendar</a></li>
+                    <li><a href="/hr/employment/">Career Opportunities</a></li>
+                    <li><a href="/ilogin/">iLogin</a></li>
+                    <li><a href="http://library.uthsc.edu/">Library</a></li>
+                    <li><a href="/redesign/map/">Maps</a></li>
+                    <li><a href="https://utap.tennessee.edu/">MyUT</a></li>
+                    <li><a href="https://news.uthsc.edu/">News</a></li>
+                    <li><a href="http://o365.uthsc.edu/">Webmail</a></li>
+                </ul>
+            </li>
+            <li><a href="/give/"><span aria-hidden="true" class="fa fa-gift"></span> Make a Gift</a></li>
+            <li><a href="/admissions/visit-uthsc.php"><span aria-hidden="true" class="fa fa-road"></span> Take a Tour</a></li>
+            <li>
+                <div class="show-for-large">
+                    <?php //include('uthsc-search-form.php'); ?>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
