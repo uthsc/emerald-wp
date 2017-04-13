4b7039c84e7c5a04dd6e776363ce37696e0df7ed
George Spake <gpspake@gmail.com>
2017-04-13 14:20:16 -0500

    update entry meta

--- emerald-wp/library/entry-meta.php
+++ emerald-wp/library/entry-meta.php
@@ -8,7 +8,9 @@
 
 if ( ! function_exists( 'foundationpress_entry_meta' ) ) :
 	function foundationpress_entry_meta() {
-		echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . sprintf( __( 'Posted on %1$s at %2$s.', 'foundationpress' ), get_the_date(), get_the_time() ) . '</time>';
-		echo '<p class="byline author">' . __( 'Written by', 'foundationpress' ) . ' <a href="' . get_author_posts_url( get_the_author_meta( 'ID' ) ) . '" rel="author" class="fn">' . get_the_author() . '</a></p>';
+        echo '<p class="byline author">' . __( 'Written by ', 'foundationpress' ) . get_the_author() . '</p>';
+        echo ' | ';
+		echo '<time class="updated" datetime="' . get_the_time( 'c' ) . '">' . get_the_date() . '</time>';
+
 	}
 endif;
