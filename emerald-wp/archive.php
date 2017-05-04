cde9c8c72a39b47ce08dd3bc3b9fdfc0e1c5d276
George Spake <gpspake@gmail.com>
2017-05-04 12:37:15 -0500

    update archive template

--- emerald-wp/archive.php
+++ emerald-wp/archive.php
@@ -17,35 +17,58 @@
 
 get_header(); ?>
 
-<div id="page" role="main">
-	<article class="main-content">
-	<?php if ( have_posts() ) : ?>
+  <div id="page" role="main">
+    <div class="row">
 
-		<?php /* Start the Loop */ ?>
-		<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', get_post_format() ); ?>
-		<?php endwhile; ?>
+      <!--Main Content-->
+      <article class="columns medium-push-2 medium-10 large-7 main-content">
+        <h1><?php the_archive_title(); ?></h1><hr>
+          <?php if (have_posts()) : ?>
+              <?php /* Start the Loop */ ?>
+              <?php while (have_posts()) : the_post(); ?>
+                  <?php get_template_part('template-parts/content', get_post_type()); ?>
+              <?php endwhile; ?>
 
-		<?php else : ?>
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
+          <?php else : ?>
+              <?php get_template_part('template-parts/content', 'none'); ?>
 
-		<?php endif; // End have_posts() check. ?>
+          <?php endif; // End have_posts() check. ?>
 
-		<?php /* Display navigation to next/previous pages when applicable */ ?>
-		<?php
-		if ( function_exists( 'foundationpress_pagination' ) ) :
-			foundationpress_pagination();
-		elseif ( is_paged() ) :
-		?>
-			<nav id="post-nav">
-				<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'foundationpress' ) ); ?></div>
-				<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'foundationpress' ) ); ?></div>
-			</nav>
-		<?php endif; ?>
+          <?php /* Display navigation to next/previous pages when applicable */ ?>
+          <?php
+          if (function_exists('foundationpress_pagination')) :
+              foundationpress_pagination();
+          elseif (is_paged()) :
+              ?>
+            <nav id="post-nav">
+              <div class="post-previous"><?php next_posts_link(__('&larr; Older posts', 'foundationpress')); ?></div>
+              <div class="post-next"><?php previous_posts_link(__('Newer posts &rarr;', 'foundationpress')); ?></div>
+            </nav>
+          <?php endif; ?>
 
-	</article>
-	<?php get_sidebar(); ?>
+      </article>
 
-</div>
+      <!--Left sidebar-->
+      <div class="columns medium-pull-10 small-4 medium-2 large-pull-7 ">
+        <aside class="" id="sticky-sidebar" data-sticky-container>
+          <div class="sticky" data-sticky data-top-anchor="sticky-sidebar" data-btm-anchor="sticky-sidebar-end:top"
+               data-sticky-on="medium">
+              <?php dynamic_sidebar('left-sidebar-widgets'); ?>
+          </div>
+        </aside>
+      </div>
+
+      <!--Right Sidebar-->
+      <div class="columns small-8 medium-11 large-3">
+        <aside class="sidebar" id="sticky-sidebar" data-sticky-container>
+          <div class="sticky" data-sticky data-top-anchor="sticky-sidebar" data-btm-anchor="sticky-sidebar-end:top"
+               data-sticky-on="large">
+              <?php get_sidebar(); ?>
+          </div>
+        </aside>
+      </div>
+
+    </div>
+  </div>
 
 <?php get_footer();
